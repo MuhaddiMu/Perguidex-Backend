6424742a59dd12f9fcbaa7fb2267161900ee6ec4
Muhaddis <muhaddisshah@gmail.com>
2021-12-20 03:30:16 +0500

    relations and tasks query

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,9 @@ class User extends Authenticatable implements HasApiTokensContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class, 'user_id');
+    }
 }
