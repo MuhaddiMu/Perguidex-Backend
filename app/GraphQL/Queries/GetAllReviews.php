612b5be165f34b721bd7daa7258da0c230a398b9
Muhaddis <muhaddisshah@gmail.com>
2021-12-26 12:23:41 +0500

    get All Reviews Query

---
+++ app/GraphQL/Queries/GetAllReviews.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\DayReview;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+class GetAllReviews
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  @param  null  $root Always null, since this field has no parent.
+     * @param  array<string, mixed>  $args The field arguments passed by the client.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Shared between all fields.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Metadata for advanced query resolution.
+     * @return mixed
+     */
+    public function __invoke($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+
+        $UserID   = $context->user()->id;
+        $Page     = ($args["page"] * $args["first"]) - $args["first"];
+        $Reviews    = DayReview::where('user_id', $UserID)->where('deleted', False)->offset($Page)->limit($args["first"])->get();
+
+        return $Reviews;
+    }
+}
