2dea43bf202aafe5f851108d32242282a30ad7ab
Muhaddis <muhaddisshah@gmail.com>
2021-12-26 11:51:51 +0500

    show all tasks

---
+++ app/GraphQL/Queries/GetAllTasks.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Task;
+use GraphQL\Type\Definition\ResolveInfo;
+use Nuwave\Lighthouse\Support\Contracts\GraphQLContext;
+
+
+class GetAllTasks
+{
+    /**
+     * Return a value for the field.
+     *
+     * @param  @param  null  $root Always null, since this field has no parent.
+     * @param  array<string, mixed>  $args The field arguments passed by the client.
+     * @param  \Nuwave\Lighthouse\Support\Contracts\GraphQLContext  $context Shared between all fields.
+     * @param  \GraphQL\Type\Definition\ResolveInfo  $resolveInfo Metadata for advanced query resolution.
+     * @return mixed
+     */
+    public function __invoke($root, array $args, GraphQLContext $context, ResolveInfo $resolveInfo)
+    {
+
+        $UserID   = $context->user()->id;
+        $Page     = ($args["page"] * $args["first"]) - $args["first"];
+        $Tasks    = Task::where('user_id', $UserID)->where('deleted', False)->offset($Page)->limit($args["first"])->get();
+
+        return $Tasks;
+    }
+}
