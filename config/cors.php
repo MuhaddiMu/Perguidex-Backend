6bdb13ee28ab00c2e4725df7f0e296471ea603d8
Muhaddis <muhaddisshah@gmail.com>
2021-12-29 01:16:01 +0500

    fix api call

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*.perguidex.io:*'],
+    'allowed_origins' => ['*.perguidex.io'],
 
     'allowed_origins_patterns' => [],
 
