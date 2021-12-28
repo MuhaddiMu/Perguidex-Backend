ee4400c4e2bb511f264e68326238f77bece7fe5b
Muhaddis <muhaddisshah@gmail.com>
2021-12-29 00:27:48 +0500

    fix trust proxy

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array|string|null
      */
-    protected $proxies = "*"; # Change Later
+    protected $proxies; #  = "*" locally
 
     /**
      * The headers that should be used to detect proxies.
