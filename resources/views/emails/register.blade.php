7ddd7f432a5ebdf18f35f64361ad8a5dcfd3de8f
Muhaddis <muhaddisshah@gmail.com>
2021-12-31 02:31:07 +0500

    update signup form

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -5,11 +5,11 @@
 </head>
 <body>
 Hi, {{$details["name"]}}<br/><br/>
-This is really exciting: Welcome (officially) to Perguidex.io. We're so
+This is really exciting: Welcome (officially) to Perguidex.io. I'm so
 lucky to have you.<br/><br/>
 I am here to help make sure you get the results you expect from Perguidex, so don't hesitate to reach out with questions. I'd love to hear from
 you.<br/><br/>
-To help you get started, I recommend start adding tasks and review your day at the end of the each day<br/><br/>
+To help you get started, I recommend start adding tasks and review your day at the end of each day<br/><br/>
 If you need support, you can reply to this email.<br/><br/>
 I can talk you through the details and information you need to get started on the
 right foot.<br/><br/>
