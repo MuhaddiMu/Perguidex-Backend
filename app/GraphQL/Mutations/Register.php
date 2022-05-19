109d5afeec269e456a734fd23da6ea5d90e3d95f
Muhaddis <muhaddisshah@gmail.com>
2022-05-19 13:02:52 +0500

    disable emails

--- app/GraphQL/Mutations/Register.php
+++ app/GraphQL/Mutations/Register.php
@@ -79,13 +79,6 @@ class Register
             throw new HasApiTokensException($user);
         }
 
-
-        \Mail::to($args["email"])->send(new \App\Mail\SignupEmail(["name" => $args["name"]]));
-
-        //  Notify the Admin About the New Signup
-        $ToEmail = env('MAIL_OWNER_EMAIL', 'muhaddisshah@gmail.com');
-        \Mail::to($ToEmail)->send(new \App\Mail\NewSignupNotification(["name" => $args["name"], "email" => $args["email"]]));
-
         return [
             'token'  => $user->createToken('default')->plainTextToken,
             'status' => 'SUCCESS',
